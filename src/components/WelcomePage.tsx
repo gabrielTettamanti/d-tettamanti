cfebc8f0f00ad839a4c6be22077da09e14ba8bbc
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-11 16:26:06 -0300

    Welcome page tittle styles

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -2,11 +2,11 @@ import '../assets/scss/WelcomePage.scss'
 
 export default function WelcomePage() {
     return (
-        <section id='welcome-page-container'>
-            <h1 id="welcome-title">
-                D. Tettamanti
-                <span>Finanzas</span>
-            </h1>
-        </section>
+      <section id="welcome-page-container">
+        <h1 id="welcome-title">
+          <p>D. Tettamanti</p>
+          <span>Finanzas</span>
+        </h1>
+      </section>
     );
 }
