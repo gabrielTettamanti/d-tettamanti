81a9fe91f210da063f81f6860e6a527b803ad10b
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-11 16:29:56 -0300

    Added calendly link

--- src/components/MeetingButton.tsx
+++ src/components/MeetingButton.tsx
@@ -1,5 +1,13 @@
 import '../assets/scss/MeetingButton.scss'
 
 export default function MeetingButton({font}: {font: any}) {
-    return <a href="https://www.youtube.com/" className={`${font} meeting-button`}>Agendar cita</a>;
+    return (
+      <a
+            href="https://calendly.com/dtettamanti/45min"
+            target='_blank'
+            className={`${font} meeting-button`}
+      >
+        Agendar cita
+      </a>
+    );
 }
