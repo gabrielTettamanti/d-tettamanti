4e775c814a8ba7ab2c148bc8567f6f7e6d89c152
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-11 19:54:33 -0300

    Added example welcome text and responsible

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -7,6 +7,9 @@ export default function WelcomePage() {
           <p>D. Tettamanti</p>
           <span>Sustentabilidad financiera</span>
         </h1>
+        <p id='welcome-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime animi placeat! Culpa quod, quos tenetur debitis adipisci totam? Tempore incidunt consectetur magni iste beatae soluta delectus officia illum praesentium!
+        </p>
       </section>
     );
 }
