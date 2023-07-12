bc865a586f77676df50cc7a9011f73f02c424eee
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-11 21:09:31 -0300

    Added arrow button

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,5 +1,8 @@
 import '../assets/scss/WelcomePage.scss'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
 export default function WelcomePage() {
     return (
       <section id="welcome-page-container">
@@ -7,9 +10,15 @@ export default function WelcomePage() {
           <p>D. Tettamanti</p>
           <span>Sustentabilidad financiera</span>
         </h1>
-        <p id='welcome-text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime animi placeat! Culpa quod, quos tenetur debitis adipisci totam? Tempore incidunt consectetur magni iste beatae soluta delectus officia illum praesentium!
+        <p id="welcome-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime
+          animi placeat! Culpa quod, quos tenetur debitis adipisci totam?
+          Tempore incidunt consectetur magni iste beatae soluta delectus officia
+          illum praesentium!
         </p>
+        <button id="arrow-down-icon">
+          <FontAwesomeIcon icon={faArrowDown} />
+        </button>
       </section>
     );
 }
