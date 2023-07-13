53fc8f47712adf3780ebdb6f76d87dec2e4d6efc
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-07-13 05:19:10 -0300

    Changing styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,8 @@ import MeetingButton from "./MeetingButton";
 
 export default function NavigationBar({ font }: { font: any }) {
   return (
-    <nav id="navigation-bar">
+    <header id="navigation-bar">
       <MeetingButton font={font} />
-    </nav>
+    </header>
   );
 }
